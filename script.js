1280f2d8bbfb4bf17f447b6f02cc8eb577de960f
Ahmedish <71420720+Ahmedish@users.noreply.github.com>
2021-12-29 16:13:07 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("#inputForm");
+const params = {
+    access_key: '076c9167c2ddfaab6a4f773eefc1193e'
+}
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const symbol = form.elements.symbol.value;
+    const shares = form.elements.numShares.value;
+    const date = form.elements.d.value;
+    const res = await axios.get(`http://api.marketstack.com/v1/tickers/${symbol}/eod/${date}`, { params });
+    const initPrice = res.data.close;
+    const res2 = await axios.get(`http://api.marketstack.com/v1/tickers/${symbol}/eod/latest`, { params });
+    const finPrice = res2.data.close;
+    let profit = shares * (finPrice - initPrice);
+    const h2 = document.querySelector("#message");
+    if (profit > 0) {
+        h2.innerHTML = `You would have made ${profit} dollars`
+    } else if (profit < 0) {
+        profit = profit * -1;
+        h2.innerHTML = `You would have lost ${profit} dollars`
+    } else {
+        h2.innerHTML = `You would have broke even`
+    }
+})
