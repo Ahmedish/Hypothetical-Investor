5524639b7aca347b039f2054cc9725b91a5bb961
Ahmedish <71420720+Ahmedish@users.noreply.github.com>
2022-01-07 00:50:30 -0500

    Required inputs for form and set minimum date input

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear() - 1;
+today = yyyy + '-' + mm + '-' + dd;
+
+document.getElementById("myDate").min = today;
+
 const form = document.querySelector("#inputForm");
 const params = {
     access_key: '076c9167c2ddfaab6a4f773eefc1193e'
